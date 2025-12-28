29741948457d747dab50318a2b00fbf27f2b76f1
Cruzer-S <yyyynoom@gmail.com>
2025-12-29 07:24:53 +0900

    update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-BASE=$(realpath ${0})
+BASE=$(dirname ${0})
 echo "BASE: ${BASE}"
 
 if [ "$1" == "Server" ]; then
@@ -8,8 +8,9 @@ if [ "$1" == "Server" ]; then
 elif [ "$1" == "Client" ]; then
 	rm -f "${BASE}/Resources/image.iso"
 	rm -f "${BASE}/Resources/image_copy.iso"
+	./setup.sh
 	cp ${BASE}/build/Client/Client ${BASE}/build/Client/Client_copy
-	${BASE}/build/Client/Client 127.0.0.1 1584 							\
-								${BASE}/Resources/image.iso				\
-								${BASE}/Resources/image_copy.iso
+	${BASE}/build/Client/Client 127.0.0.1 1584 			\
+				    ${BASE}/Resources/image.iso		\
+				    ${BASE}/Resources/image_copy.iso
 fi
